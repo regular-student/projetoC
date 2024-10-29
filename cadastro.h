bb098fc86ae6d5176673c531869948e6f0ca12ec
Lucas de Albuquerque <lucasdealbuc@gmail.com>
2024-10-29 12:38:07 -0300

    Atualização 1.0

---
+++ cadastro.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void menu_principal(){
+
+    printf("\n          MENU PRINCIPAL        ");
+    printf("\n  [1] - Clientes                ");
+    printf("\n  [2] - Serviços                ");
+    printf("\n  [3] - SAIR                    ");
+    printf("\n\n");
+}
--- main.c
+++ main.c
@@ -1,8 +1,49 @@
+//Bibliotecas Padrão
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
+//Final Bibliotecas Padrão
+
+//Minhas Bibliotecas
+#include "cadastro.h"
+//Final Minhas Bibliotecas
 
 int main() {
-
+	//Definir linguagem para portugues
+    setlocale(LC_ALL, "portuguese");
+	//Declaracao de variaveis
+	int verific_inicial;
+	char entrada;
+	
+	//altera o valor para o funcionamento do "DO WHILE"
+	verific_inicial = 1;
+	
+	do{
+		menu_principal();
+		entrada = getch();
+		
+		switch(entrada){
+			case '1' : //Parte dos Clientes
+				printf ("Em constru��o...");
+				printf("\n\n");
+				break;
+			
+			case '2' : //Parte dos Servi�os
+				printf ("Trabalhando nisso ainda...");
+				printf ("\n\n");
+				break;
+			
+			case '3' :
+				verific_inicial = 0;
+				break;
+				
+			default :
+				printf ("\n		Op��o invalida				");
+				printf ("\n	Esolha outra op��o para continuar	");	
+				printf ("\n\n")	;
+			}
+			
+	}while (verific_inicial != 0);
 
     return 0;
 }
